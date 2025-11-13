491b2e1bf67855248a0569f58bb634bb95f22b3b
Arthur <a.arthuraraujo1711@gmail.com>
2025-11-13 09:38:02 -0300

    feat: Criando pagina inicial de login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
+import LoginPage from "./routes/LoginPage";
 
 
 function App() {
-  
+  const [isLogged, setIsLogged] = useState(false);
+
   return (
-    <>
-      
-    </>
-  )
+    <div className="min-h-screen">
+      {isLogged ? (
+        <HomePage onLogout={() => setIsLogged(false)} />
+      ) : (
+        <LoginPage onLoginSuccess={() => setIsLogged(true)} />
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/routes/BemEstar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BemEstar = () => {
+  return (
+    <div>BemEstar</div>
+  )
+}
+
+export default BemEstar
