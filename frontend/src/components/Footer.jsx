ae45fbf1e5e4a84b9071b89f475bb09b89e53a61
Breno <brenogsbao@gmail.com>
2025-11-16 18:06:28 -0300

    style(footer): Mudando a cor do footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full bg-blue-600 text-white text-center py-3 mt-auto">
+    <footer className="w-full bg-blue-950 text-white text-center py-3 mt-auto">
       © 2025 — Todos os direitos reservados.
     </footer>
   );
